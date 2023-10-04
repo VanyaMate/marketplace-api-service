6460eab6fedf0968d22accd52695fe7d9076360e
VanyaMate <buhu653@mail.ru>
2023-10-04 12:35:17 +0300

    update ts config, remove js files

--- config/errors.config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NOT_FOUND = exports.NO_VALID_DATA = void 0;
-exports.NO_VALID_DATA = 'No valid data';
-exports.NOT_FOUND = 'Not found';
