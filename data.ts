ffbd6fe3d7ae352b361a0ea8335d876ac4813c6e
VanyaMate <buhu653@mail.ru>
2023-12-03 04:03:33 +0300

    add brands and companies to export

--- data.ts
+++ data.ts
@@ -1,3 +1,5 @@
 export * from './data/products/products_1.json';
 export * from './data/products/products_2.json';
 export * from './data/categories/categories.json';
+export * from './data/brands/brands_1.json';
+export * from './data/companies/companies_1.json.json';
